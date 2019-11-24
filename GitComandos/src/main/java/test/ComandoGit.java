e01c3778ab735391e71ff9cc2ecd5b962da9696f
Izac <izacjunior@gmail.com>
2019-11-24 13:28:31 -0300

    COMANDO GIT

--- GitComandos/src/main/java/test/ComandoGit.java
+++ GitComandos/src/main/java/test/ComandoGit.java
@@ -10,7 +10,7 @@ public class ComandoGit {
 		//git status - VERIFICAR STATUS DO GIT
 		// git add - ADD NO REPOSITORIO LOCAL 
 		//git commit -m "" - ADD REPOSITORIO DO GIT
-		//git pull - PEGAR DE DIFERENTE DO SERVIDOR GIT 
+		//git pull - PEGAR DE DIFERENTE DO SERVIDOR GIT --
 		//git push - SUBIR CODIGO PARA O SERVIDOR
 
 	}
