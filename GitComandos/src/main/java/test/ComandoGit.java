78e1ba7e28a1dcdfb76ab6a83001f718ea6bf0ad
Izac <izacjunior@gmail.com>
2019-11-24 13:21:19 -0300

    resolvendo

--- GitComandos/src/main/java/test/ComandoGit.java
+++ GitComandos/src/main/java/test/ComandoGit.java
@@ -1,20 +1,18 @@
 package test;
 
-public class TestProjetoGit {
-	
-	public static void main(String args[]) {
-		//comando git 
+public class ComandoGit {
+
+	public static void main(String[] args) {
+		//comando basicos git 
 		
 		// git init -INICIALIZAR GIT NO DIRETORIO
 		// git remote - SUBIR CODIGO NO GITHUB
-	
 		//git status - VERIFICAR STATUS DO GIT
 		// git add - ADD NO REPOSITORIO LOCAL 
 		//git commit -m "" - ADD REPOSITORIO DO GIT
 		//git pull - PEGAR DE DIFERENTE DO SERVIDOR GIT 
 		//git push - SUBIR CODIGO PARA O SERVIDOR
-		
-		
+
 	}
 
 }
