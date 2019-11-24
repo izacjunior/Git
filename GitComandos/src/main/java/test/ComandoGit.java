187a7ed00d848c58cc5960878ef8bbe73ded0693
Izac <izacjunior@gmail.com>
2019-11-24 18:21:25 -0300

    add

--- GitComandos/src/main/java/test/ComandoGit.java
+++ GitComandos/src/main/java/test/ComandoGit.java
@@ -6,7 +6,7 @@ public class ComandoGit {
 		//comando basicos git 
 		
 		// git init -INICIALIZAR GIT NO DIRETORIO
-		// git remote - SUBIR CODIGO NO GITHUB
+		// git remote add origin - SUBIR CODIGO NO GITHUB
 		//git status - VERIFICAR STATUS DO GIT
 		// git add - ADD NO REPOSITORIO LOCAL 
 		//git commit -m "" - ADD REPOSITORIO DO GIT
